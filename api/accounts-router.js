4bc9b6778017eeb4f93646d0e683bb134bd1d857
Eric Ferguson <turtle0888@gmail.com>
2020-02-17 16:47:40 -0600

    Add POST, PUT, DELETE to accounts

--- api/accounts-router.js
+++ api/accounts-router.js
@@ -14,4 +14,57 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db('accounts')
+    .where({ id })
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'failed to retrieve account' });
+    });
+});
+
+router.post('/', (req, res) => {
+  db('accounts')
+    .insert(req.body, 'id')
+    .then(inserted => {
+      res.status(201).json(inserted);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'failed to add account' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  db('accounts')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'failed to update account' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  db('accounts')
+    .where({ id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'failed to remove account' });
+    });
+});
+
 module.exports = router;
